ad22de346bdcfd1135ffeaa02fbbb67c8dad808d
1morshed1 <morshedfahim87@gmail.com>
2025-06-22 20:13:28 +0600

    feat: finish password_manager program

--- password-manager/password_manager.py
+++ password-manager/password_manager.py
@@ -16,24 +16,23 @@ def load_key():
     file.close()
     return key
 
-key = load_key()
+key = load_key() 
 
-master_pwd = input("What is the master password? ")
-
+fer = Fernet(key)
 
 def view():
     with open("G:\\python-projects\\password-manager\\passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:",user,", Password: ", passw)
+            print("User:",user,", Password: ", fer.decrypt(passw.encode()).decode())
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open("G:\\python-projects\\password-manager\\passwords.txt", "a") as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     mode  = input("Would you like to add a new password or view existing ones (view, add)? Press q to quit. ").lower()
