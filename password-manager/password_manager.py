b65fc590f95be9af3afb1f39b961ed0b365e868b
1morshed1 <morshedfahim87@gmail.com>
2025-06-22 19:44:42 +0600

    feat: install and import cryptography package

--- password-manager/password_manager.py
+++ password-manager/password_manager.py
@@ -1,1 +1,35 @@
-pwd = input("What is the master password? ")
+
+from cryptography.fernet import Fernet
+
+master_pwd = input("What is the master password? ")
+
+def view():
+    with open("G:\\python-projects\\password-manager\\passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:",user,", Password: ", passw)
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("G:\\python-projects\\password-manager\\passwords.txt", "a") as f:
+        f.write(name + "|" + pwd + "\n")
+
+while True:
+    mode  = input("Would you like to add a new password or view existing ones (view, add)? Press q to quit. ").lower()
+
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+
+    elif mode == "add":
+        add()
+
+    else:
+        print("Invalid Mode.")
+        continue
+
