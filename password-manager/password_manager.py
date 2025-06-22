509d6dd2b801d8a06644ede1c3ebab3988a3c37a
1morshed1 <morshedfahim87@gmail.com>
2025-06-22 20:03:02 +0600

    feat: generate key.key file

--- password-manager/password_manager.py
+++ password-manager/password_manager.py
@@ -1,7 +1,25 @@
 
 from cryptography.fernet import Fernet
 
+'''
+The code snippet below is used to generate key
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("G:\\python-projects\\password-manager\\key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open("G:\\python-projects\\password-manager\\key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+
 master_pwd = input("What is the master password? ")
+
 
 def view():
     with open("G:\\python-projects\\password-manager\\passwords.txt", "r") as f:
